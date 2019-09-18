4d906d630dcc6a00eadd5f2b6342e604522caf12
Autopsia <sectordefectuoso@gmail.com>
2019-09-18 12:52:41 -0500

    Commit inicial

---
+++ Miriana/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Miriana
+//
+//  Created by Alumno on 9/18/19.
+//  Copyright © 2019 Sector Defectuoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
